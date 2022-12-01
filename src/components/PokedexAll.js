f6a47c00eb71427e9dabd4fa3322b1bd27009fdc
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2022-12-01 16:11:42 +0100

    PokedexAll now successfully links to PokedexIndividual

--- src/components/PokedexAll.js
+++ src/components/PokedexAll.js
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getAllPokemon } from './lib/api';
 import PokedexCard from './PokedexCard';
 
 const PokedexAll = () => {
   const [pokemons, setPokemons] = useState(null);
+
   useEffect(() => {
     getAllPokemon()
       .then((res) => {
@@ -17,16 +17,10 @@ const PokedexAll = () => {
     return <p>Loading</p>;
   }
 
-  const titleCasePokemons = pokemons.results.map((pokemon) => {
-    const first = pokemon.name.toString().charAt(0).toUpperCase();
-    const second = pokemon.name.toString().slice(1);
-    return first + second;
-  });
-
   return (
     <div className="container has-text-centered">
-      {titleCasePokemons.map((pokemon) => (
-        <PokedexCard key={pokemon} pokemon={pokemon} />
+      {pokemons.results.map((pokemon) => (
+        <PokedexCard key={pokemon.name} pokemon={pokemon.name} />
       ))}
     </div>
   );
