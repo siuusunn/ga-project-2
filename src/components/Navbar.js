7154b30013c93a84fe62188cd354c46d14e63af5
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2022-12-01 12:14:21 +0100

    Added Navbar.js with basic navbar features

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar is-danger">
+        <div className="container">
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+            <Link to="/" className="navbar-item">
+              Pokedex
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
